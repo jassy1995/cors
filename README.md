6373333c3d80476f1a2e37629596a19872fc3170
jassy1995 <babatundejoseph85@gmail.com>
2021-08-17 13:49:16 +0100

    i created the GrantRequest class with it method

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 ..............HOW TO USE CORS-HELPER.............
 STEP 1:In the root folder of your file run this command composer require prof/cors-helper
 STEP 2: require CorsHelper at the top(above all your code) e.g use CorsHelper\CorsHelper
-STEP 3: Call function like this CorsHelper::cors();
+STEP 3: Call function like this CorsHelper::GrantRequest();
 This is how the default set up look like,you did not need to do this below set up,am just showing you how the default set up look like.
 .................THIS IS THE DEFAULT SETUP....................
-CorsHelper::cors([
+CorsHelper::GrantRequest([
 'origin'=>['All origin'],
 'method'=>['POST','GET','DELETE','PUT','PATCH','OPTIONS'],
 'header'=>['All header'],
@@ -12,16 +12,16 @@ CorsHelper::cors([
 ])
 ..............FULL DEFAULT EXAMPLE.....................
 use CorsHelper\CorsHelper;
-CorsHelper::cors();
+CorsHelper::GrantRequest();
 So simple cors is enable and you can start your work without any cors problem.
 NOTE:if you want to customize cors-helper,please read through the instructions below
-For Origin only : CorsHelper::cors(['origin'=>['http://example1.com','http://example2.com']]);
-For method only : CorsHelper::cors(['method'=>['POST','GET']]);
-For header only : CorsHelper::cors(['header'=>['Content-Type','Authorization']]);
-For max-age only : CorsHelper::cors(['maxAge'=>600]); // any number of your choice
+For Origin only : CorsHelper::GrantRequest(['origin'=>['http://example1.com','http://example2.com']]);
+For method only : CorsHelper::GrantRequest(['method'=>['POST','GET']]);
+For header only : CorsHelper::GrantRequest(['header'=>['Content-Type','Authorization']]);
+For max-age only : CorsHelper::GrantRequest(['maxAge'=>600]); // any number of your choice
 ..............FULL CUSTOMIZE EXAMPLE............
 use CorsHelper\CorsHelper;
-CorsHelper::cors([
+CorsHelper::GrantRequest([
 'origin'=>['http://example1.com','http://example2.com'],
 'method'=>['POST','GET'],
 'header'=>['Content-Type','Authorization'],
--- src/CorsHelper.php
+++ src/CorsHelper.php
@@ -1,68 +1,119 @@
 <?php
 namespace CorsHelper;
 class CorsHelper{
-
-  public static function cors($data=[]) {
-     $verifyOrigin = "";
-    if(isset($data['origin'])){
-      $arrLength = count($data['origin']);
-      for($x = 0; $x < $arrLength; $x++) {
-        $verifyOrigin = $data['origin'][$x];
+  public static function GrantRequest($data=[])
+  {
+    $myInfo=(new CorsHelper)->ifEmpty();
+    $myHeaders = getallheaders();
+    if(isset($_SERVER['HTTP_ORIGIN']) && isset($_SERVER['REQUEST_METHOD']) && isset($myHeaders)){
+        
+      if(!empty($data['origin'])){
+        $origin = (new CorsHelper)->handleOrigin($data['origin']);
+        $myInfo['origin'] = $origin;
+      }
+      if(!empty($data['maxAge'])){
+        $maxAge =(new CorsHelper)->handleMaxAge($data['maxAge']); 
+        $myInfo['maxAge'] = $maxAge;
+      }
+       if(!empty($data['header'])){
+        $header =(new CorsHelper)->handleHeaders($data['header']); 
+        $myInfo['header'] = $header; 
+      }
+       if(!empty($data['method'])){
+        $method =(new CorsHelper)->handleMethods($data['method']);
+        $myInfo['method'] = $method;
+      }
+       if(!empty($data['contentType'])){
+         $contentType =(new CorsHelper)->handleContentType($data['contentType']);
+        $myInfo['contentType'] = $data['contentType'];
+      }
+      if(!empty($data['credentials'])){
+        $credentials =(new CorsHelper)->handleCredential($data['credentials']);
+        $myInfo['credentials'] = $data['credentials'];
+      }
     }
+  
+    (new CorsHelper)->setAccess($myInfo['origin'], $myInfo['method'], $myInfo['header'], $myInfo['contentType'],$myInfo['credentials'],$myInfo['maxAge']);
+  }
+  //SET UP THE CORS VALUE
+  public function setAccess($url,$method,$header,$contentType,$credentials,$maxAge){
+    if(isset($_SERVER['HTTP_ORIGIN']) && isset($_SERVER['REQUEST_METHOD']) ){
+      header("Access-Control-Allow-Origin: $url");
+      header("Access-Control-Allow-Methods: $method");
+      header("Access-Control-Allow-Headers: $header");
+      header("Content-Type: $contentType");
+      header("Access-Control-Allow-Credentials: $credentials");
+      header("Access-Control-Max-Age: $maxAge");
     }
-    if($_SERVER['HTTP_ORIGIN'] == $verifyOrigin){
+  } 
+ //DEFAULT SETUP
+   public function ifEmpty(){
     $url=  '*';
     $method = 'POST,GET,DELETE,PUT,PATCH,OPTIONS';
     $credentials = true;
     $maxAge =72800;
-    $header = '*';
-  
-   
-          if(isset($data['origin'])){
-            $url = implode(', ', $data['origin']);
-          }
-  
-          if(isset($data['method'])){
-            $method = implode(', ', $data['method']);
-           }
-  
-          if(isset($data['credentials'])){
-              $credentials = $data['credentials'];
-           }
-  
-           if(isset($data['maxAge'])){
-                $maxAge = $data['maxAge'];
-           }
-  
-          if(isset($data['header'])){
-               $header = implode(', ', $data['header']);
-          }
-  
-  
-      if(isset($_SERVER['HTTP_ORIGIN'])){
-              header("Access-Control-Allow-Origin: $url");
-              header("Access-Control-Allow-Credentials: $credentials");
-              header("Access-Control-Max-Age: $maxAge");
-              header('Content-Type: text/plain');
-              
-  
-      }
-      if($_SERVER['REQUEST_METHOD'] == 'OPTIONS'){
-          if(isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])){
-              header("Access-Control-Allow-Methods: $method");
-          }
-          if(isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])){
-              header("Access-Control-Allow-Headers: $header");
-          } 
-          }
-      header('Content-Type: application/json');
-    }else{
-      die("You are not allowed");
-    }
-  
-      
+    $header = 'X-Requested-With,token,Authorization,X-Auth-Token,Origin,Content-Type,Cache-Control, Pragma,Accept, Accept-Encoding';
+    $contentType= 'text/plain';
+    return(['origin'=>$url, 'method'=>$method,'header'=>$header,'contentType'=>$contentType,'credentials'=> $credentials, 'maxAge'=>$maxAge]);
+}
+//HANDLE ORIGIN
+public function handleOrigin($origin){
+  if(isset($_SERVER['HTTP_ORIGIN'])){
+    $currentOrigin = $_SERVER['HTTP_ORIGIN'];
+    if(gettype($origin)=='string' && $currentOrigin == $origin) return $origin;
+    elseif (gettype($origin)=='array'){
+      $index = array_search($currentOrigin, $origin);
+        return $origin[$index];
+    } 
   }
 }
-
+  //HANDLE HEADER
+  public function handleHeaders($header){
+    $allHeaders = ['X-Requested-With','token','Authorization','X-Auth-Token','Origin','Content-Type','Cache-Control','Pragma','Accept','Accept-Encoding'];
+    $response = "";
+    if(gettype($header)=='string'){
+      array_push($allHeaders,$header);
+      $response = array_unique($allHeaders);
+    }elseif(gettype($header)=='array') $response = array_unique(array_merge($allHeaders,$header));
+    return implode(', ', $response);
+  }
+  //HANDLE METHOD
+  public function handleMethods($method){
+    $currentMethod = $_SERVER['REQUEST_METHOD'];
+    $response = "";
+    $allMethod = ['POST','GET','DELETE','PUT','PATCH','OPTIONS'];
+    if(gettype($method)=='string' && in_array($method,$allMethod)){
+      if($currentMethod == $method) $response = $method;
+      else throw new \Exception('Request does not allow');
+    }
+    if(gettype($method)=='array' && !empty(array_intersect($allMethod,$method))){
+       $index = array_search($currentMethod, $method);
+       if($index > -1) $response = implode(', ', $method);
+       else  throw new \Exception('Request does not allow');   
+    }
+    return $response;
+  }
+  //HANDLE CONTENT-TYPE
+  public function handleContentType($contentType){
+    $response = "";
+    if(gettype($contentType)=='string')  $response = $contentType;
+    return $response;
+  }
+  //HANDLE CREDENTIAL
+  public function handleCredential($credentials){
+    $response = "";
+    if(gettype($credentials)=='string'){
+      $response = filter_var($credentials,FILTER_VALIDATE_BOOLEAN);
+    }elseif(gettype($credentials)=='boolean') $response = $credentials;
+    return $response;
+  }
+  //HANDLE MAX-AGE
+  public function handleMaxAge($age){
+    $response = "";
+    if(gettype($age)=='string') $response = intval($age);
+    elseif(gettype($age)=='array')   $response = intval($age[0]);
+    else $response = $age;
+    return $response;
+  }
+}
 ?>
-
